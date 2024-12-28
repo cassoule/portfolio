629f651b88f086d04a9d6ea586b41fcdb1b9ee89
milo <milo.grv@gmail.com>
2024-12-28 21:20:11 +0100

    rotating text nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://milogourvest.netlify.app",
-  integrations: [preact()]
+  integrations: [preact(), tailwind(), vue()]
 });
